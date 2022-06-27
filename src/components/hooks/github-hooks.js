6464490f9f00d9cf65eeeddad487520e2a6a60fa
Breno <brenodveronezi@gmail.com>
2022-06-26 21:14:51 -0300

    final commit

---
+++ src/components/hooks/github-hooks.js
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+import { GithubContext } from "../../providers/github-providers";
+
+const useGithub = () => {
+    const { } = useContext( GithubContext);
+
+    return {};
+};
+
+export default useGithub;
